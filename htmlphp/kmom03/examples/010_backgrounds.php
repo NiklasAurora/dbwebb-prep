2d20baab821856a6ed649aa6594e9caa2f521ad5
Niklas Aurora Andersson <nhdandersson@gmail.com>
2020-09-14 16:06:44 +0200

    Updated example for HT20

---
+++ htmlphp/kmom03/examples/010_backgrounds.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backgrounds</title>
+    <link rel="stylesheet" href="style/010_style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>I am Aurora</h1>
+                <p>And I'm a Developer</p>
+            </div>
+        </div>
+        <nav class="navbar">
+            <a href="#">Home</a>
+            <a href="#">Report</a>
+            <a href="#">About</a>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <header>
+                <h2>Aurora</h2>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis id repellendus, quam cum explicabo laudantium vero impedit recusandae repudiandae cumque. Ea eius assumenda saepe dolore minus, omnis ratione odio autem?</p>
+            <footer>
+                <p>Written by Aurora</p>
+            </footer>
+        </article>
+    </main>
+    <footer class="site-footer">
+        <a href="#">Unicorn</a>
+        <a href="#">HTML5</a>
+        <a href="#">CSS3</a>
+    </footer>
+</body>
+
+</html>
